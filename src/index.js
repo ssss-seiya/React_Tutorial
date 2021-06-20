204c3cfc41fd5e63dcea3615a16e9ba098d85885
seiya_t <kenose0328@gmail.com>
2021-06-20 10:44:28 +0900

    6.ゲーム勝者の判定

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ class Board extends React.Component {
   handleClick(i) {
     // squares を直接変更する代わりに、.slice() を呼んで配列のコピーを作成
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -39,7 +42,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -78,6 +87,28 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+// winnerを返す
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    // [a, b, c]が[X, X, X]若しくは[O, O, O]だった場合、XかOを返す
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
